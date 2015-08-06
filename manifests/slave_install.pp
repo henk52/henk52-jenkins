19426897262b3a4ab73c955609c9550e73d654b5
henk52 <henk52@users.noreply.github.com>
2015-08-06 05:59:47 +0200

    Update slave_install.pp

--- manifests/slave_install.pp
+++ manifests/slave_install.pp
@@ -1,7 +1,7 @@
 # Get the masters public key via Hiera.
 
 $szHomeDirectory = "/var/lib/jenkins"
-$szMastersPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCucLk2z55RZ/Ms0Vs/Ms/LqzdwhH2hDq1P253nlJxTKbRkSCrfsgLakoPWRBP/7QKkBOJUy47/w6sArXbyiTqZW3N+CEsDkp+sbH7Nzv9+8ILCvU4XdsETiWGG07o+u2gqWsNOYiFaS6CG6Mp0IZHnoEKRv/dNI49i8PGMX/NKBcLRR7/XTfWXKEe4O983b/cC8gpIVo+AI0dWsLYqi5PMaJCspexPOq8guNj9BEFyRc7oTorcKS7sO27NZzDnWyk833uKbgV32vcu5gIAzxT/lrpiHKbXfyIt61uaz3tEhZN8O48+RPx7gjklfYE+1mNOmvZHpKqDmt39BWxk82e9 jenkins@master"
+$szMastersPublicKey = hiera("MastersPublicKey")
 
 package {'ssh': ensure => present }
 package {'openjdk-7-jre-headless': ensure => present }
