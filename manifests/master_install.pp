1639bdf96703d8a400bda57e29592643880e37d8
Your Name <you@example.com>
2015-12-23 10:31:26 +0100

    Adding plugin installations.

--- manifests/master_install.pp
+++ manifests/master_install.pp
@@ -1,7 +1,7 @@
 # Get the masters public key via Hiera.
 
 $szHomeDirectory = "/var/lib/jenkins"
-
+$szJenkinsPluginDir = "$szHomeDirectory/plugins"
 
 $szSshPackageName = "openssh"
 
@@ -14,6 +14,12 @@ package { 'git': ensure => present }
 #package { 'jenkins': ensure => present }
 package { "$szJavaPkgName": ensure => present }
 
+service { 'sshd': 
+  ensure => running,
+  enable => true,
+  require => Package["$szSshPackageName"],
+}
+
 exec { 'get_jenkins_rpm':
   command => 'wget http://dm/storage/jenkins-1.625.3-1.1.noarch.rpm',
   cwd     => '/tmp',
@@ -21,6 +27,7 @@ exec { 'get_jenkins_rpm':
   path => '/usr/bin',
 }
 
+# Install Jenkins
 exec { 'jenkins':
   command => 'rpm -ih /tmp/jenkins-1.625.3-1.1.noarch.rpm',
   creates => '/var/lib/jenkins',
@@ -28,13 +35,6 @@ exec { 'jenkins':
   require => Exec['get_jenkins_rpm'],
 }
 
-service { 'sshd': 
-  ensure => running,
-  enable => true,
-  require => Package["$szSshPackageName"],
-}
-
-# Install Jenkins
 service { 'jenkins': 
   ensure => running,
   enable => true,
@@ -42,7 +42,6 @@ service { 'jenkins':
 #  require => Package['jenkins',"$szJavaPkgName"],
 }
 
-# Install Jenkins plugins.
 
 exec { 'jenkins_rsa':
   creates  => "$szHomeDirectory/.ssh/id_rsa",
@@ -54,3 +53,85 @@ exec { 'jenkins_rsa':
                 Exec[ 'jenkins' ],
               ],
 }
+
+# Install Jenkins plugins.
+# TODO get the list of plugins from hiera.
+exec { 'greenballs_plugin':
+  creates => "$szJenkinsPluginDir/greenballs.hpi",
+  command => "wget http://dm/storage/jenkins/greenballs.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+# Generate usage graphs for the jenknis server and nodes.
+# You can access it at: :8080/monitoring and :8080/monitoring/nodes
+exec { 'monitoring_plugin':
+  creates => "$szJenkinsPluginDir/monitoring.hpi",
+  command => "wget http://dm/storage/jenkins/monitoring.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+exec { 'disk-usage_plugin':
+  creates => "$szJenkinsPluginDir/disk-usage.hpi",
+  command => "wget http://dm/storage/jenkins/disk-usage.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+# required by build-flow-plugin
+exec { 'buildgraph-view_plugin':
+  creates => "$szJenkinsPluginDir/buildgraph-view.hpi",
+  command => "wget http://dm/storage/jenkins/buildgraph-view.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+# Allow configuration of flows and parallel tasks.
+exec { 'build-flow-plugin_plugin':
+  creates => "$szJenkinsPluginDir/build-flow-plugin.hpi",
+  command => "wget http://dm/storage/jenkins/build-flow-plugin.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+# required by test-results-analyzer
+exec { 'junit_plugin':
+  creates => "$szJenkinsPluginDir/junit.hpi",
+  command => "wget http://dm/storage/jenkins/junit.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+# 
+exec { 'testng-plugin_plugin':
+  creates => "$szJenkinsPluginDir/testng-plugin.hpi",
+  command => "wget http://dm/storage/jenkins/testng-plugin.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
+# Allow tracking of test results.
+exec { 'test-results-analyzer_plugin':
+  creates => "$szJenkinsPluginDir/test-results-analyzer.hpi",
+  command => "wget http://dm/storage/jenkins/test-results-analyzer.hpi",
+  cwd     => "$szJenkinsPluginDir",
+  path    => '/usr/bin',
+  require => Exec['jenkins'],
+  notify  => Service['jenkins'],
+}
+
